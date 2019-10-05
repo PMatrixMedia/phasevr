914ef30e830984298f239619d8a8277b7f6057bd
pmatrix <pmatrixmedia@gmail.com>
2019-10-05 09:26:56 -0500

    Tweaked UI Nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {MatMenuModule} from '@angular/material/menu';
+import { TargetBlankDirective } from './target-blank.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhasenavComponent
+    PhasenavComponent,
+    TargetBlankDirective
   ],
   imports: [
     BrowserModule,
